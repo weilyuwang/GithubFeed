a3c1f5a13ffaba7a3c9cb8e208ba3e3c5fd5034e
weilyuwang <weilyuwang@gmail.com>
2020-08-28 17:11:21 -0400

    :fire: login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from 'styled-components'
+
+import Login from './Login'
+
+import 'styled-components/macro'
+
+const Global = createGlobalStyle({
+  body: {
+    backgroundColor: '#fff',
+    color: '#444',
+    fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto"',
+    padding: 0,
+    margin: 0,
+    borderTop: '4px solid rgb(210, 54, 105)',
+  },
+  '*': {
+    boxSizing: 'border-box',
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Global />
+      <Login />
+    </>
   );
 }
 
